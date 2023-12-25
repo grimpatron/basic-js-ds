a475693588e3239c8eafd19a3883de8dece6122d
Mikhail Khilman <Hellhammer220bpm@gmail.com>
2023-12-25 19:17:36 +0300

    feat: finish task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,48 +1,132 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
+Implement simple binary search tree according to task 
+description using Node from extensions
 */
+
+
 class BinarySearchTree {
 
+  constructor() {
+    this.rootNode = null;
+  }
+
+  // Возвращает корневой Нода.
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Добавление элемента в дерево.
+  add(data) {
+    const node = this.rootNode;
+    if (node === null) {
+      this.rootNode = new Node(data);
+    } else {
+      const searchTree = function(node) {
+        if (data < node.data) {
+          if (node.left === null) { node.left = new Node(data);
+          } else return searchTree(node.left);
+        } 
+        if (data > node.data) {
+          if (node.right === null) { node.right = new Node(data);
+          } else return searchTree(node.right);
+        } else return null;
+      };
+      return searchTree(node);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Поиск элемента в дереве и возврат его
+  find(data) {
+    let current = this.rootNode;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else current = current.right;
+      
+      if (current === null) return null;
+    }
+    return current;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Поиск элемента в дереве
+  has(data) {
+    let current = this.rootNode;
+    while (current) {
+      if (data === current.data) return true;
+      if (data < current.data) {
+        current = current.left;
+      } else current = current.right;
+    }
+    return false;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Удаление элемента из дерева
+  remove(data) {
+    const removeNode = function(node, data) {
+      if (data === node.data) {
+        // Нода не имеет потомков
+        if (node.left === null && node.right === null || node === null) {
+          return null;
+        }
+
+        // Нода имеет только одного потомка
+        if (node.left === null) return node.right;
+        if (node.right === null) return node.left;
+
+        // Нода имеет двух потомков
+        let tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.rootNode = removeNode(this.rootNode, data);
   }
+
+
+  // Поиск максимального элемента в дереве.
+  max() {
+    let current = this.rootNode;
+    // let current = this.minmax((this.rootNode).right);
+    while (current.right !== null) {
+      current = current.right;
+    }
+
+    // return current.current.right ;
+    return current.data;
+  }
+
+  // minmax(cur) {
+  //   while (cur !== null) {
+  //     current = cur;
+  //   }
+  //   return current;
+  // }
+
+  // Поиск минимального элемента в дереве
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.rootNode;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
   }
 }
+
+
 
 module.exports = {
   BinarySearchTree
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function removeKFromList(l, k) {
+
+  let listNode = new ListNode();
+
+  let curList = listNode;
+  curList.next = l;
+
+
+  while (curList.next) {
+    if (curList.next.value === k) {
+      // обновляет указатель на начало списка
+      curList.next = (curList.next).next;
+    } else {
+      curList = curList.next;
+    }
+  }
+
+  return listNode.next;
 }
 
 module.exports = {
   removeKFromList
 };
+
+// https://medium.com/@shehanPW/remove-k-from-list-codesignal-algorithms-problem-regarding-linked-lists-897a3f39e48f#:~:text=Given%20a%20singly%20linked%20list%20of%20integers%20l,%3D%20%5B1%2C%202%2C%203%2C%204%2C%205%2C%206%2C%207%5D.
